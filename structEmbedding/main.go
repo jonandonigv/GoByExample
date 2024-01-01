2e0d9b15621608992d50cfb8128439184c717d9b
Jon Andoni Galdos <jonandonigv@gmail.com>
2024-01-01 18:00:58 +0100

    structEmbedding

--- structEmbedding/main.go
+++ structEmbedding/main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+type base struct {
+	num int
+}
+
+func (b base) describe() string {
+	return fmt.Sprintf("base with num=%v", b.num)
+}
+
+type container struct {
+	base
+	str string
+}
+
 func main() {
-	fmt.Println("Hello this is a test")
+	c := container{
+		base: base{num: 1},
+		str:  "the number is 1",
+	}
+	fmt.Println(c)
+	fmt.Println(base{num: 1}.describe())
+	fmt.Println(c.describe())
 }
