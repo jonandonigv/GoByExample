2ee138d6df18674ef68986528d9e5442aa385fd7
Jon Andoni Galdos <jonandonigv@gmail.com>
2024-01-04 08:49:23 +0100

    mutex

--- mutexes/main.go
+++ mutexes/main.go
@@ -5,12 +5,18 @@ import (
 	"sync"
 )
 
+// Container holds a map of counters; we add a Mutex
+// to update it concurrently from multiple goroutines
 type Container struct {
 	mu       sync.Mutex
 	counters map[string]int
 }
 
+// Mutexes must not be copied, so when passed we do it as a pointer
 func (c *Container) inc(name string) {
+	// Lock the mutex before accesing counters
+	// Unlock it at the end of the function using
+	// defer statement
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.counters[name]++
@@ -19,6 +25,8 @@ func (c *Container) inc(name string) {
 
 func main() {
 	c := Container{
+		// Note that zero value if a muter ix usable as-is
+		// so no initizalization is required
 		counters: map[string]int{"a": 0, "b": 0},
 	}
 	var wg sync.WaitGroup
