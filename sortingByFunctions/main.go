a373040e8b90c8f8237ea776ac37a0af21c2133f
Jon Andoni Galdos <jonandonigv@gmail.com>
2024-01-04 16:48:31 +0100

    changes

--- sortingByFunctions/main.go
+++ sortingByFunctions/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"cmp"
+	"fmt"
+	"slices"
+)
+
 func main() {
+	fruits := []string{"peach", "banana", "kiwi"}
+
+	lenCmp := func(a, b string) int {
+		return cmp.Compare(len(a), len(b))
+	}
+
+	slices.SortFunc(fruits, lenCmp)
+	fmt.Println(fruits)
+
+	type Person struct {
+		name string
+		age  int
+	}
+
+	people := []Person{
+		{name: "Jax", age: 37},
+		{name: "James", age: 56},
+		{name: "Sam", age: 26},
+	}
+
+	slices.SortFunc(people, func(a, b Person) int {
+		return cmp.Compare(a.age, b.age)
+	})
+
+	fmt.Println(people)
 
 }
