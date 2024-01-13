f13e02a287e1858b43ed5872961f5588e5eae681
Jon Andoni Galdos <jonandonigv@gmail.com>
2024-01-13 20:01:05 +0100

    w

--- writingFiles/main.go
+++ writingFiles/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "os"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func check(e error) {
 	if e != nil {
@@ -17,4 +21,20 @@ func main() {
 	check(err)
 
 	defer f.Close()
+
+	d2 := []byte{115, 111, 109, 101, 10}
+	n2, err := f.Write(d2)
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n2)
+
+	n3, err := f.WriteString("writes\n")
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n3)
+
+	f.Sync()
+
+	w := bufio.NewWriter(f)
+	n4, err := w.WriteString("buffered\n")
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n4)
 }
