34e1e0129feb5e365044d16da72bc5fd08302190
Jon Andoni Galdos <jonandonigv@gmail.com>
2024-01-18 18:07:38 +0100

    +

--- commandLine/main.go
+++ commandLine/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -15,5 +16,22 @@ func main() {
 	fmt.Println(argsWithProg)
 	fmt.Println(argsWithoutProg)
 	fmt.Println(arg)
+
+	//Flags
+	wordPtr := flag.String("word", "foo", "a string")
+
+	numbePtr := flag.Int("num", 42, "an int")
+	forkPtr := flag.Bool("fork", false, "a bool")
+
+	var svar string
+	flag.StringVar(&svar, "svar", "bar", "a string var")
+
+	flag.Parse()
+
+	fmt.Println("word:", *wordPtr)
+	fmt.Println("num:", *numbePtr)
+	fmt.Println("fork:", *forkPtr)
+	fmt.Println("svar:", svar)
+	fmt.Println("tail:", flag.Args())
 
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/commandline
+
+go 1.21.5
