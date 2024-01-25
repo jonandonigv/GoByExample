4aac2ba36c91478a846e9fcb752087fed0bbeaf9
Jon Andoni Galdos <jonandonigv@gmail.com>
2024-01-25 04:05:00 +0100

    +

--- processes/spawn/main.go
+++ processes/spawn/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os/exec"
 )
 
@@ -30,5 +31,22 @@ func main() {
 	grepCmd := exec.Command("grep", "hello")
 
 	grepIn, _ := grepCmd.StdinPipe()
+	grepOut, _ := grepCmd.StdoutPipe()
+	grepCmd.Start()
+	grepIn.Write([]byte("hello grep\ngoodby grep"))
+	grepIn.Close()
+	grepBytes, _ := io.ReadAll(grepOut)
+	grepCmd.Wait()
+
+	fmt.Println("> grep hello")
+	fmt.Println(string(grepBytes))
+
+	lsCmd := exec.Command("bash", "-c", "ls -a -l -h")
+	lsOut, err := lsCmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("> ls -a -l -h")
+	fmt.Println(string(lsOut))
 
 }
